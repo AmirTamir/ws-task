f0363fdd78b63301f7f96b041e1476aff20a9e36
Amir Tamir <amir25144@gmail.com>
2023-11-20 14:56:23 +0200

    removed comments

--- server/index.js
+++ server/index.js
@@ -1,31 +1,3 @@
-// const ws = require("ws");
-// const server = new ws.Server({ port: 3000 });
-
-// server.on("connection", (socket) => {
-//   socket.on("message", (message) => {
-//     console.log(message);
-//     socket.send(`${message}`);
-//   });
-// });
-
-// const io = require("socket.io");
-// const server = io(8080);
-
-// server.on("connection", (socket) => {
-//   console.log("New WebSocket connection established");
-
-//   setInterval(() => {
-//     const mockData = {
-//       timestamp: Date.now(),
-//       value: Math.random() * 100,
-//     };
-
-//     socket.emit("data", mockData);
-//   }, 1000);
-// });
-
-// console.log("WebSocket server listening on port 8080");
-
 const express = require("express");
 const app = express();
 const http = require("http");
